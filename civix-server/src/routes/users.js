fe2aee72ecc3b60d7c174610d192a8c9b4a535c1
Saurav Kumar <zoroduke007@gmail.com>
2026-02-20 22:17:38 +0530

    feat: Add VITE_API_URL and VITE_MAPBOX_TOKEN environment variables.

--- civix-server/src/routes/users.js
+++ civix-server/src/routes/users.js
@@ -7,10 +7,12 @@ const { protect } = require('../middleware/auth');
 router.put('/bookmark/:id', protect, toggleBookmark);
 router.get('/bookmarks', protect, getBookmarks);
 
+const upload = require('../middleware/upload');
+
 // Social Routes
 router.put('/follow/:id', protect, followUser);
 router.put('/unfollow/:id', protect, unfollowUser);
 router.get('/:id', getUserProfile);
-router.put('/profile', protect, updateUserProfile);
+router.put('/profile', protect, upload.single('avatar'), updateUserProfile);
 
 module.exports = router;
